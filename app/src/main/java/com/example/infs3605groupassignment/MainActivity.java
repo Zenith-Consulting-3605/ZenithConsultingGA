aa2539c86348dc11a6e186f957316857e734ff3e
Truman Ng <ng.truman@gmail.com>
2020-06-27 22:05:29 +1000

    MainActivity: - Created general UI layout - Linked SignUp and Home Pages

--- app/src/main/java/com/example/infs3605groupassignment/MainActivity.java
+++ app/src/main/java/com/example/infs3605groupassignment/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.infs3605groupassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button signIn;
+    private Button signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signIn = findViewById(R.id.btnLogin);
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Home.class));
+            }
+        });
+
+        signUp = findViewById(R.id.btnSignUp);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SignUp.class));
+            }
+        });
     }
 }
