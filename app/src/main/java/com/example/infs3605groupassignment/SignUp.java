d4b7fe74f22def02ceb2b36b4e400665016392b7
RyanDuong360 <ryanduong360@gmail.com>
2020-06-29 18:27:17 +1000

    Update SignUp.java

--- app/src/main/java/com/example/infs3605groupassignment/SignUp.java
+++ app/src/main/java/com/example/infs3605groupassignment/SignUp.java
@@ -1,14 +1,71 @@
 package com.example.infs3605groupassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class SignUp extends AppCompatActivity {
+
+    DbHelper db;
+    private EditText firstName, lastName, email, password;
+    private Button signup;
+    private RadioGroup userType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        db = new DbHelper(this);
+
+        firstName = findViewById(R.id.edtFirstName);
+        lastName = findViewById(R.id.edtLastName);
+        email = findViewById(R.id.edtEmail);
+        password = findViewById(R.id.edtPassword);
+        signup = findViewById(R.id.btnSignUp);
+        userType = findViewById(R.id.rgUserType);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fn = firstName.getText().toString();
+                String ln = lastName.getText().toString();
+                String em = email.getText().toString();
+                String ps = password.getText().toString();
+
+                //RadioButton checkBtn = findViewById(userType.getCheckedRadioButtonId());
+                //String utv = checkBtn.getText().toString();
+
+
+                if(firstName.equals("")||lastName.equals("")||email.equals("")||password.equals("")){
+                    Toast.makeText(getApplicationContext(),"Please fill in empty fields", Toast.LENGTH_SHORT).show();
+                }else {
+                    if (em.equals(em)) {
+                        Boolean checkEmail = db.checkEmail(em);
+                        if (checkEmail == true) {
+                            Boolean insert = db.insert(fn, ln, em, ps);
+                            if (insert == true) {
+                                Toast.makeText(getApplicationContext(), "Register Successful", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        else {
+                            Toast.makeText(getApplicationContext(), "Email Already Exists", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                    else{}
+                }
+
+            }
+        });
+
     }
 }
