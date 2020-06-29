db1e0c0d4dd8f13305fdc9e764e9d96c31ac3735
RyanDuong360 <ryanduong360@gmail.com>
2020-06-29 19:02:26 +1000

    Update MainActivity.java

--- app/src/main/java/com/example/infs3605groupassignment/MainActivity.java
+++ app/src/main/java/com/example/infs3605groupassignment/MainActivity.java
@@ -6,11 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    DbHelper db;
     private Button signIn;
     private Button signUp;
+    private EditText email, password;
 
     private Button btn;
 
@@ -19,11 +23,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        db = new DbHelper(this);
+
+        email = findViewById(R.id.edtEmail);
+        password = findViewById(R.id.edtPassword);
+
+
         signIn = findViewById(R.id.btnLogin);
         signIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Home.class));
+                String em = email.getText().toString();
+                String ps = password.getText().toString();
+                Boolean checkLogin = db.loginCheck(em,ps);
+
+                if(checkLogin==true){
+                    Toast.makeText(getApplicationContext(),"Successful Login", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(MainActivity.this, Home.class));
+                }else{
+                    Toast.makeText(getApplicationContext(),"Invalid Email or Password", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
 
