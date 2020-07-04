50bbaa82da542083651c36559562bf828d39c027
Amy Lai <z5215512@ad.unsw.edu.au>
2020-07-05 01:07:16 +1000

    bottom navigation bars

--- app/src/main/java/com/example/infs3605groupassignment/Project.java
+++ app/src/main/java/com/example/infs3605groupassignment/Project.java
@@ -4,18 +4,48 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.infs3605groupassignment.Profile.Profile;
 import com.example.infs3605groupassignment.Profile.ProfileDetail;
 
 public class Project extends AppCompatActivity {
     public static final String CODE_EXTRA = "CODE_EXTRA";
     private TextView trial;
 
+    private ImageView home;
+    private ImageView project;
+    private ImageView profile;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project);
+
+        home = findViewById(R.id.imvHome);
+        project = findViewById(R.id.imvProject);
+        profile = findViewById(R.id.imvProfile);
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Project.this, Home.class));
+            }
+        });
+        project.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Project.this, Project.class));
+            }
+        });
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Project.this, Profile.class));
+            }
+        });
 
         //WILL BE ERASED
         Intent intent = getIntent();
